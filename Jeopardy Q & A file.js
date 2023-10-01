12d75a11423446d25d3099d9ca220c787861a086
Chris <118864776+darthchris623@users.noreply.github.com>
2023-09-30 22:09:08 -0400

    Update Jeopardy Q & A file.js
    Removed the "showing" key/value pair because they weren't being used.

--- Jeopardy Q & A file.js
+++ Jeopardy Q & A file.js
@@ -2,61 +2,91 @@ const categories = [
     {
         category: 'Ancient Civilizations',
         clues: [
-            { question: 'This city was founded in 750 B.C. by two brothers, Remus and Romulus.', answer: 'Rome', showing: null },
-            { question: 'Located in Mexico, this civilization made a calendar that ended in 2012.', answer: 'Mayan', showing: null },
-            { question: 'This civilization invented the first writing system.', answer: 'Sumerian', showing: null },
-            { question: 'Democracy comes from this civilization.', answer: 'Greek', showing: null },
-            { question: 'This civilization is known for making the first code of laws.', answer: 'Babylonian', showing: null }
+            { question: 'This city was founded in 750 B.C. by two brothers, Remus and Romulus.', answer: 'Rome' },
+            { question: 'Located in Mexico, this civilization made a calendar that ended in 2012.', answer: 'Mayan' },
+            { question: 'This civilization invented the first writing system.', answer: 'Sumerian' },
+            { question: 'Democracy comes from this civilization.', answer: 'Greek' },
+            { question: 'This civilization is known for making the first code of laws.', answer: 'Babylonian' }
         ]
     },
     {
         category: 'U.S. History',
         clues: [
-            { question: 'He was the first U.S. president.', answer: 'George Washington', showing: null },
-            { question: 'This president freed the slaves in 1865.', answer: 'Abraham Lincoln', showing: null },
-            { question: 'This president was in a wheelchair due to polio.', answer: 'Franklin Roosevelt', showing: null },
-            { question: 'This territory was bought from Russia in 1867.', answer: 'Alaska', showing: null },
-            { question: 'This city was the first capital of the U.S.', answer: 'Philadelphia', showing: null }
+            { question: 'He was the first U.S. president.', answer: 'George Washington' },
+            { question: 'This president freed the slaves in 1865.', answer: 'Abraham Lincoln' },
+            { question: 'This president was in a wheelchair due to polio.', answer: 'Franklin Roosevelt' },
+            { question: 'This territory was bought from Russia in 1867.', answer: 'Alaska' },
+            { question: 'This city was the first capital of the U.S.', answer: 'Philadelphia' }
         ]
     },
     {
         category: 'The Solar System',
         clues: [
-            { question: 'This planet is the closest to the sun.', answer: 'Mercury', showing: null },
-            { question: 'This planet has a big red spot that is a storm.', answer: 'Jupiter', showing: null },
-            { question: 'This planet is red due to iron oxide.', answer: 'Mars', showing: null },
-            { question: 'This planet spins backward compared to the other planets.', answer: 'Venus', showing: null },
-            { question: 'This planet has the most water in the solar system.', answer: 'Earth', showing: null },
+            { question: 'This planet is the closest to the sun.', answer: 'Mercury' },
+            { question: 'This planet has a big red spot that is a storm.', answer: 'Jupiter' },
+            { question: 'This planet is red due to iron oxide.', answer: 'Mars' },
+            { question: 'This planet spins backward compared to the other planets.', answer: 'Venus' },
+            { question: 'This planet has the most water in the solar system.', answer: 'Earth' },
         ]
     },
     {
         category: 'Geography',
         clues: [
-            { question: 'This country has 11 time zones.', answer: 'Russia', showing: null },
-            { question: 'This country has a canal that connects the Atlantic and Pacific oceans.', answer: 'Panama', showing: null },
-            { question: 'This asian country is made up of over 17,000 islands', answer: 'Indonesia', showing: null },
-            { question: 'This country is completely situated between Russia and China.', answer: 'Mongolia', showing: null },
-            { question: 'This African river is the longest in the world.', answer: 'The Nile', showing: null }
+            { question: 'This country has 11 time zones.', answer: 'Russia' },
+            { question: 'This country has a canal that connects the Atlantic and Pacific oceans.', answer: 'Panama' },
+            { question: 'This asian country is made up of over 17,000 islands', answer: 'Indonesia' },
+            { question: 'This country is completely situated between Russia and China.', answer: 'Mongolia' },
+            { question: 'This African river is the longest in the world.', answer: 'The Nile' }
         ]
     },
     {
         category: 'Pop Culture',
         clues: [
-            { question: 'She sang Party In The U.S.A.', answer: 'Miley Cyrus', showing: null },
-            { question: 'Leonardo DiCaprio and Kate Winslet starred in this movie.', answer: 'Titanic', showing: null },
-            { question: 'Robert Downey Jr played this Marvel superhero.', answer: 'Iron Man', showing: null },
-            { question: 'Matt Damon played this spy.', answer: 'Jason Bourne', showing: null }, 
-            { question: 'John Krasinski played this action hero.', answer: 'Jack Ryan', showing: null }
+            { question: 'She sang Party In The U.S.A.', answer: 'Miley Cyrus' },
+            { question: 'Leonardo DiCaprio and Kate Winslet starred in this movie.', answer: 'Titanic' },
+            { question: 'Robert Downey Jr played this Marvel superhero.', answer: 'Iron Man' },
+            { question: 'Matt Damon played this spy.', answer: 'Jason Bourne' }, 
+            { question: 'John Krasinski played this action hero.', answer: 'Jack Ryan' }
         ]
     },
     {
         category: 'Art',
         clues: [
-            { question: 'He painted the last supper of Jesus.', answer: 'Leonardo Da Vinci', showing: null },
-            { question: 'He painted the Sistine Chapel.', answer: 'Michelangelo', showing: null },
-            { question: 'He painted the Starry Night.', answer: 'Vincent van Gogh', showing: null },
-            { question: 'The Thinker was sculpted by this man.', answer: 'Auguste Rodin', showing: null },
-            { question: 'He painted the Nymphéas, also known as the Water Lillies.', answer: 'Claude Monet', showing: null }
+            { question: 'He painted the last supper of Jesus.', answer: 'Leonardo Da Vinci' },
+            { question: 'He painted the Sistine Chapel.', answer: 'Michelangelo' },
+            { question: 'He painted the Starry Night.', answer: 'Vincent van Gogh' },
+            { question: 'The Thinker was sculpted by this man.', answer: 'Auguste Rodin' },
+            { question: 'He painted the Nymphéas, also known as the Water Lillies.', answer: 'Claude Monet' }
+        ]
+    },
+    {
+        category: 'Hungarian History',
+        clues: [
+            { question: 'This empire occupied Hungary from 1541 to 1699.', answer: 'Ottoman' },
+            { question: 'This king who ruled for 3 years, was litterally killed by his throne in 1063.', answer: 'Bela I' },
+            { question: 'In 1241, Hungary was attacked by this empire.', answer: 'Mongolian' },
+            { question: 'He established the first printing press in 1472 in Buda.', answer: 'Andras Hess' },
+            { question: 'In 1521, this fortress in present-day Belgrade fell to the Turks.', answer: 'Nandorfehervar' }
+        ]
+    },
+    {
+        category: 'Literature',
+        clues: [
+            { question: 'He wrote "A Christmas Carol in 1843.', answer: 'Charles Dickens' },
+            { question: 'He wrote "War And Peace in 1867.', answer: 'Leo Tolstoy' },
+            { question: 'He wrote "Les Misérables" in 1862.', answer: 'Victor Hugo' },
+            { question: 'He worte "From The Earth To The Moon" in 1865.', answer: 'Jules Verne' },
+            { question: 'She wrote "Pride And Prdjudice" in 1813.', answer: 'Jane Austen' }
+        ]
+    }
+    {
+        category: 'Chemistry',
+        clues: [
+            { question: 'Two atoms of oxygen and one atom of hydrogen make this molecule.', answer: 'Water' },
+            { question: 'One atom of carbon and two atoms of oxygen make this molecule.', answer: 'Carbon dioxide' },
+            { question: 'Protons and neutrons make up the nucleus, while these orbit the nucleus.', answer: 'Electrons' },
+            { question: 'An atom that can have a varying number of neutrons is called this.', answer: 'Isotope' },
+            { question: 'An atom with a different number of protons than electrons is called this.', answer: 'Ion' }
         ]
     }
 ];
